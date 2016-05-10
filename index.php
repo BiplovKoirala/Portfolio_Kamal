8bf73f24b8b772da2259773d0c51163b5dc4e7da
Biplov Koirala <koiralabiplov@gmail.com>
2016-05-10 15:11:38 +0545

    Update index.php

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
  *     development
  *     testing
  *     production
- *
+ *	pull request done by biplov koirala
  * NOTE: If you change these, also change the error_reporting() code below
  */
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
